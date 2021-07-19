fae3619363127c32260534ba60749123ca37ac84
Andreas Gnyp <andreas@gnyp.de>
2021-07-19 10:03:51 +0200

    update.sh: add env-test to migrations

--- update.sh
+++ update.sh
@@ -4,13 +4,18 @@ set -e
 
 cd /home/agnyp/dev/clark/application
 echo Running in $(pwd) ...
+
 echo checkout master ...
 git co master
+
 echo pull from origin ...
 git pull
 bundle
 yarn
-rake db:migrate
+
+echo "migrating dev && test"
+rake db:migrate && RAILS_ENV=test rake db:migrate
 git co -
 
+echo changing directory back ...
 cd -
