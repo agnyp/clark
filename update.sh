da7280f4f2e6b7889630964c999974d5a57f04df
Andreas Gnyp <andreas@gnyp.de>
2021-07-20 11:13:10 +0200

    make update.sh more noticable with echos

--- update.sh
+++ update.sh
@@ -3,19 +3,22 @@
 set -e
 
 cd /home/agnyp/dev/clark/application
-echo Running in $(pwd) ...
+echo [Script]Running in $(pwd) ...
 
-echo checkout master ...
+echo [Script]checkout master ...
 git co master
 
-echo pull from origin ...
+echo
+echo [Script]pull from origin ...
 git pull
 bundle
 yarn
 
-echo "migrating dev && test"
+echo
+echo "[Script]migrating dev && test"
 rake db:migrate && RAILS_ENV=test rake db:migrate
 git co -
 
-echo changing directory back ...
+echo
+echo [Script]changing directory back ...
 cd -
