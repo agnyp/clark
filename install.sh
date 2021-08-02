9d0eebb329961dd2e5f7bc2a1f5c0b38d7e51202
Andreas Gnyp <andreas@gnyp.de>
2021-08-02 11:16:21 +0200

    install.sh mod/add env-vars

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-export DOMAIN=0.0.0.0:3040    # configure CMS and bind it to this address
+# export DOMAIN=0.0.0.0:3030    # configure CMS and bind it to this address
+export DOMAIN=localhost:3030    # configure CMS and bind it to this address
 export SKIP_DEPENDENCIES=true # you may want to skip installing gems, postgres, etc.
 export DROP_DB=true           # drop previously created DB before (a must unless a first installation)
 export APP_LOCALE=de          # configure app to run for a specific locale (e.g Austria)
+export CUSTOM_SEEDS=true          # configure app to run for a specific locale (e.g Austria)
 
 ./bin/install
